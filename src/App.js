6c24c92cbd2ecbbff65d2dacad97d50a3d8e7d2a
DarkWister <darkwistercrak17@gmail.com>
2024-11-25 17:46:12 -0400

    tested alerts

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import './App.css';
 import handleSubmit from './handler/handlesubmit';
+import { successAlert, failureAlert } from './utils/alerts';
 import { useRef } from 'react';
  
 function App() {
-  const dataRef = useRef()
+  const username = useRef();
+  const password = useRef();
  
-  const submithandler = (e) => {
-    e.preventDefault()
-    handleSubmit()
-    dataRef.current.value = ""
-  }
+    const submithandler = (e) => {
+      e.preventDefault();
+      handleSubmit(username.current.value, password.current.value).then(() =>{
+        successAlert("USUARIO REGISTRADO");
+      }).catch((error) => {
+        failureAlert("ERROR AL REGISTRAR USUARIO");
+      })
+      username.current.value = ""
+      password.current.value = ""
+    }
  
   return (
     <div className="App">
       <form onSubmit={submithandler}>
-        <input type= "text" ref="User"/>
-        <input type= "text" ref="Password"/>
+        <input type= "text" ref={username}/>
+        <input type= "text" ref={password}/>
         <button type = "submit">Save</button>
+        
       </form>
     </div>
   );
--- src/handler/handlesubmit.js
+++ src/handler/handlesubmit.js
@@ -1,16 +1,16 @@
 import { addDoc, collection } from "@firebase/firestore"
 import { firestore } from "../services/firebase"
  
-const handleSubmit = (user, password) => {
+const handleSubmit = async (user, password) => {
     const ref = collection(firestore, "usuarios") // Firebase creates this automatically
  
     let data = {
         user: user,
         password: password
-    }
+    };
     
     try {
-        addDoc(ref, data)
+        await addDoc(ref, data);
     } catch(err) {
         console.log(err)
     }
